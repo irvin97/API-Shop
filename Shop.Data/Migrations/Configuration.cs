92a579957d5cd18218754fdf1a3c0add4caafd59
irvin97 <irvin97@users.noreply.github.com>
2019-12-03 23:03:42 +0700

    add Intergrate-AspnetIdentity

--- Shop.Data/Migrations/Configuration.cs
+++ Shop.Data/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 ﻿namespace Shop.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Shop.Model.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -16,8 +19,31 @@
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ShopDbContext()));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new ShopDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "admin",
+                Email = "admin@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "Admin"
+
+            };
+            manager.Create(user, "123456$");
+            if(!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
+
+            var adminUser = manager.FindByEmail("admin@gmail.com");
+
+           
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
         }
     }
 }
